82057f6e0e324d0bfed0290007b73cbf0a54714a
Matteo Alessani <alessani@gmail.com>
2013-03-04 17:05:02 +0100

    creation user on pgsql

--- recipes/postgresql.rb
+++ recipes/postgresql.rb
@@ -1,52 +1,32 @@
 gem_package "pg"
 
-postgresql_connection_info = {:host => "127.0.0.1", :port => 5432, :username => 'postgres', :password => node['postgresql']['password']['postgres']}
+postgresql_connection_info = {:host => "127.0.0.1", :port => 5432, :username => 'postgres', :password => Chef::EncryptedDataBagItem.load("dbusers", "postgres")['password_root']}
 
-pgdata = data_bag_item("dbusers", "misshobby")
+pgusers = data_bag("postgresql")
 
-# if pgdata['database']
-#   mysql_database pgdata['database'] do
-#     connection postgresql_connection_info
-#     action :create
-#   end
-# end
+
+# do the same but pass the provider to the database resource
+pgusers.each do |user|
+  user = Chef::EncryptedDataBagItem.load('postgresql', user)
   
-# mysql_database_user pgdata['name'] do
-#   connection postgresql_connection_info
-#   password pgdata['password']
-#   database_name pgdata['database'] if pgdata['database']
-#   host 'localhost'
-#   action :grant
-# end
-
-r = package "ruby-pg" do
-  package_name "libpgsql-ruby"
-  action :nothing
-end
-r.run_action(:upgrade)
-
-# Snippet from opscode to reload gems
-require 'rubygems'
-Gem.clear_paths
-require "pg"
-
-execute "create-database-user" do
-  command "createuser -U postgres -SDRw #{pgdata['name']}"
-  only_if do
-    c = PGconn.connect(:user => 'postgres', :dbname => 'postgres')
-    r = c.exec("SELECT COUNT(*) FROM pg_user WHERE usename='#{pgdata['name']}'")
-    r.entries[0]['count'] == "0"
+  postgresql_database user['database'] do
+    connection postgresql_connection_info
+    action :create
   end
-end
 
-pgdata['database'].each do |db|
-  execute "create-database-#{db}" do
-    command "createdb -U postgres -O #{pgdata['name']} -E utf8 -T template0 #{db}"
-    only_if do
-      c = PGconn.connect(:user => 'postgres', :dbname => 'postgres')
-      r = c.exec("SELECT COUNT(*) FROM pg_database WHERE datname='#{db}'")
-      #Chef::Log.info("----------------\n\nMinchia #{r.entries[0]['count']} per #{db}\n\n--------------")
-      r.entries[0]['count'] == "0"
-    end
+  postgresql_database_user user['id'] do
+    connection postgresql_connection_info
+    password user['password']
+    action :create
   end
+
+  # grant all privileges on all tables in foo db
+  postgresql_database_user user['id'] do
+    connection postgresql_connection_info
+    database_name user['database']
+    privileges [:all]
+    action :grant
+  end
+
+
 end
