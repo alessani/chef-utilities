d3aeb497bef0d1b7116f895528a847c728c6329f
Matteo Alessani <alessani@gmail.com>
2013-03-09 16:09:35 +0100

    added dependent development header packages

--- recipes/apache2_passenger.rb
+++ recipes/apache2_passenger.rb
@@ -1,3 +1,7 @@
+package "libcurl4-openssl-dev"
+package "apache2-threaded-dev"
+package "libapr1-dev"
+
 rbenv_gem "passenger" do
   ruby_version "#{node[:rubymachine][:default]}"
   gem_binary "/opt/rbenv/versions/#{node[:rubymachine][:default]}/bin/gem"
