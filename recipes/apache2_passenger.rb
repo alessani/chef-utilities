e9da60faf472eec34f9aa9f6b54fbd7d1179a572
Matteo Alessani <alessani@gmail.com>
2013-03-09 15:36:11 +0100

    added recipe for passenger for apache2

--- recipes/apache2_passenger.rb
+++ recipes/apache2_passenger.rb
@@ -4,9 +4,8 @@ rbenv_gem "passenger" do
   version "4.0.0.rc4"
 end
 
-rbenv_rehash "Doing the rehash dance"
 
-rbenv_script "install passenger for apache2" do
-  rbenv_version "#{node[:rubymachine][:patched_rubies].first}-perf"
-  code "passenger-install-apache2-module"
+
+execute "install passenger for apache2" do
+  command "rbenv shell #{node[:rubymachine][:patched_rubies].first}-perf; passenger-install-apache2-module"
 end
