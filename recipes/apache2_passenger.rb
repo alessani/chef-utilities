21159264f9b92c40abfb401ac3d173a47cf7ef88
Matteo Alessani <alessani@gmail.com>
2013-03-09 16:33:16 +0100

    passenger

--- recipes/apache2_passenger.rb
+++ recipes/apache2_passenger.rb
@@ -2,12 +2,30 @@ package "libcurl4-openssl-dev"
 package "apache2-threaded-dev"
 package "libapr1-dev"
 
+ruby_ver = node[:rubymachine][:default]
+passenger_version = "4.0.0.rc4"
+
 rbenv_gem "passenger" do
-  ruby_version "#{node[:rubymachine][:default]}"
-  gem_binary "/opt/rbenv/versions/#{node[:rubymachine][:default]}/bin/gem"
-  version "4.0.0.rc4"
+  ruby_version ruby_ver
+  gem_binary "/opt/rbenv/versions/#{ruby_ver}/bin/gem"
+  version passenger_version
 end
 
-execute "install passenger 4.0.rc4 for apache2" do
-  command "/opt/rbenv/versions/#{node[:rubymachine][:default]}/bin/passenger-install-apache2-module --auto"
+execute "install passenger 4.0.0.rc4 for apache2" do
+  command "/opt/rbenv/versions/#{ruby_ver}/bin/passenger-install-apache2-module --auto"
+end
+
+template "/etc/apache2/mods-available/passenger.load" do
+  source "passenger.load.erb"
+  mode 0644
+  owner "root"
+  group "root"
+  variables(
+    :ruby_version => ruby_ver,
+    :passenger_version => passenger_version
+  )
+end
+
+link "/etc/apache2/mods-available/passenger.load" do
+  to "/etc/apache2/mods-enabled/passenger.load"
 end
