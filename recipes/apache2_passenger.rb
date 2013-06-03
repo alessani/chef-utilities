d2c31817b858bdea903f161e872eb79051696ca9
Matteo Alessani <alessani@gmail.com>
2013-06-03 13:11:14 +0200

    removed dependencies from ruby ver

--- recipes/apache2_passenger.rb
+++ recipes/apache2_passenger.rb
@@ -6,6 +6,13 @@ ruby_ver = node[:rbenv][:rubies].first
 passenger_version = node[:passenger][:version]
 rbenv_path = "/usr/local/rbenv/versions"
 
+ruby_main_ver = case ruby_ver[0..2]
+  when "1.9"
+    "1.9.1"
+  when "1.8"
+    "1.8"
+  end
+
 rbenv_gem "passenger" do
   rbenv_version ruby_ver
   #gem_binary "/opt/rbenv/versions/#{ruby_ver}/bin/gem"
@@ -14,7 +21,7 @@ end
 
 execute "install passenger #{passenger_version} for apache2" do
   command "#{rbenv_path}/#{ruby_ver}/bin/passenger-install-apache2-module --auto"
-  not_if {::File.exists?("#{rbenv_path}/#{ruby_ver}/lib/ruby/gems/1.9.1/gems/passenger-#{passenger_version}/libout/apache2/mod_passenger.so")}
+  not_if {::File.exists?("#{rbenv_path}/#{ruby_ver}/lib/ruby/gems/#{ruby_main_ver}/gems/passenger-#{passenger_version}/libout/apache2/mod_passenger.so")}
 end
 
 template "/etc/apache2/mods-available/passenger.load" do
@@ -25,7 +32,8 @@ template "/etc/apache2/mods-available/passenger.load" do
   variables(
     :ruby_version => ruby_ver,
     :passenger_version => passenger_version,
-    :rbenv_path => rbenv_path
+    :rbenv_path => rbenv_path,
+    :ruby_main_ver => ruby_main_ver
   )
 end
 
