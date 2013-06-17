c9a103f300c6676d82704cb477cbc1c3e3697bca
Matteo Alessani <alessani@gmail.com>
2013-06-17 16:45:28 +0200

    added postgres update conf

--- recipes/postgresql.rb
+++ recipes/postgresql.rb
@@ -159,3 +159,11 @@ setup_items.each do |setup|
   end # End DB setup
 
 end
+
+template "#{node['postgresql']['dir']}/pg_hba.conf" do
+  source "pg_hba.conf.erb"
+  owner "postgres"
+  group "postgres"
+  mode 00600
+  notifies :reload, 'service[postgresql]', :immediately
+end
